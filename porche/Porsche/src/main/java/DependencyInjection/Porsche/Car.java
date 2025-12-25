be2d19b7fdfc5973550d7ae585132d367cb69cd0
skarm <skarm@DEEPAK_SIVA>
2025-12-25 17:28:18 +0530

    Initial

---
+++ porche/Porsche/src/main/java/DependencyInjection/Porsche/Car.java
@@ -0,0 +1,16 @@
+package DependencyInjection.Porsche;
+
+public class Car {
+	
+	private Wheel wheelObj; //Reference variable of wheel class or  wheel object
+	
+	public Wheel getWheelObj() {
+		return wheelObj;
+		
+	}
+	
+	public void setWheelObj(Wheel wheelObj) {
+		this.wheelObj = wheelObj;
+	}
+
+}
---
+++ porche/Porsche/src/main/java/DependencyInjection/Porsche/Wheel.java
@@ -0,0 +1,14 @@
+package DependencyInjection.Porsche;
+
+public class Wheel {
+	
+	private int wheelsize;
+	
+	public int getWheelsize() {
+		return wheelsize;
+	}
+	
+	public void setWheelsize(int wheelsize) {
+		this.wheelsize = wheelsize;	}
+
+}
